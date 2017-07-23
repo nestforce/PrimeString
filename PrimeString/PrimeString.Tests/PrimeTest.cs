994dd0d0512ded8db55f04f5d0e155a89d164903
Scotwallace.Chen83 <Scotwallace.Chen@titansoft.com.sg>
2017-07-23 10:41:14 +0800

    Added another method to solve the problem.

--- PrimeString/PrimeString.Tests/PrimeTest.cs
+++ PrimeString/PrimeString.Tests/PrimeTest.cs
@@ -26,6 +26,29 @@ namespace PrimeString.Tests
 
             Assert.AreEqual(true, kata.PrimeString("abba"));
 
+            Assert.AreEqual(false, kata.PrimeString("vvv"));
+
+            Assert.AreEqual(false, kata.PrimeString("xxxxx"));
+
+            Assert.AreEqual(true, kata.PrimeString2("abac"));
+
+            Assert.AreEqual(false, kata.PrimeString2("abab"));
+
+            Assert.AreEqual(false, kata.PrimeString2("aaaa"));
+
+            Assert.AreEqual(true, kata.PrimeString2("x"));
+
+            Assert.AreEqual(true, kata.PrimeString2("abc"));
+
+            Assert.AreEqual(false, kata.PrimeString2("fdsyffdsyffdsyffdsyffdsyf"));
+
+            Assert.AreEqual(true, kata.PrimeString2("utdutdtdutd"));
+
+            Assert.AreEqual(true, kata.PrimeString2("abba"));
+
+            Assert.AreEqual(false, kata.PrimeString2("vvv"));
+
+            Assert.AreEqual(false, kata.PrimeString2("xxxxx"));
         }
     }
 }
--- PrimeString/PrimeString/Program.cs
+++ PrimeString/PrimeString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace PrimeString
 {
@@ -7,9 +8,12 @@ namespace PrimeString
         public static void Main(string[] args)
         {
             var kata = new Kata();
-            string input = Console.ReadLine();
-            bool result = kata.PrimeString(input);
-            Console.WriteLine(result);
+            string input = "abac";
+            bool result1 = kata.PrimeString(input);
+            bool result2 = kata.PrimeString2(input);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.ReadLine();
         }
     }
 
@@ -18,22 +22,55 @@ namespace PrimeString
         public bool PrimeString(string s)
         {
             //coding and coding..
-
             if (s.Length == 1)
                 return true;
 
-            for (int i = 1; i <= s.Length / 2; i++)
+            for (var i = 1; i <= s.Length / 2; i++)
             {
                 if (s.Length % i != 0)
                     continue;
 
-                string subStr = s.Substring(0, i);
-
-                string[] split = s.Split(new[] { subStr }, StringSplitOptions.RemoveEmptyEntries);
+                var subStr = s.Substring(0, i);
+                var split = s.Split(new[] { subStr }, StringSplitOptions.RemoveEmptyEntries);
                 if (split.Length == 0)
                     return false;
             }
             return true;
+        }
+
+        public bool PrimeString2(string s)
+        {
+            if (s.Length == 1)
+                return true;
+
+            var isAnyDifferent = IsAnyDuplicatedCharacter(s);
+            if (!isAnyDifferent)
+                return false;
+
+            for (var i = 0; i <= s.Length; i++)
+            {
+                var subStr = s.Substring(0, i);
+                if (subStr == string.Empty)
+                    continue;
+
+                var times = Regex.Matches(Regex.Escape(s), subStr).Count;
+                if (times > 1 && times * subStr.Length == s.Length)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool IsAnyDuplicatedCharacter(string str)
+        {
+            var charArray = str.ToCharArray();
+            var isAnyDifferent = false;
+            for (var i = 1; i < charArray.Length; i++)
+            {
+                if (charArray[i] != charArray[0])
+                    isAnyDifferent = true;
+            }
+
+            return isAnyDifferent;
         }
     }
 }
