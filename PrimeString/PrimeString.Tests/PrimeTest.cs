5497b3331a064e0bb385de2eb1b34487aa73e87f
Scotwallace.Chen83 <Scotwallace.Chen@titansoft.com.sg>
2017-07-23 09:58:38 +0800

    Initial Commit.

---
+++ PrimeString/PrimeString.Tests/PrimeTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace PrimeString.Tests
+{
+    [TestFixture]
+    public class PrimeTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new Kata();
+
+            Assert.AreEqual(true, kata.PrimeString("abac"));
+
+            Assert.AreEqual(false, kata.PrimeString("abab"));
+
+            Assert.AreEqual(false, kata.PrimeString("aaaa"));
+
+            Assert.AreEqual(true, kata.PrimeString("x"));
+
+            Assert.AreEqual(true, kata.PrimeString("abc"));
+
+            Assert.AreEqual(false, kata.PrimeString("fdsyffdsyffdsyffdsyffdsyf"));
+
+            Assert.AreEqual(true, kata.PrimeString("utdutdtdutd"));
+
+            Assert.AreEqual(true, kata.PrimeString("abba"));
+
+        }
+    }
+}
---
+++ PrimeString/PrimeString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrimeString
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var kata = new Kata();
+            string input = Console.ReadLine();
+            bool result = kata.PrimeString(input);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Kata
+    {
+        public bool PrimeString(string s)
+        {
+            //coding and coding..
+
+            if (s.Length == 1)
+                return true;
+
+            for (int i = 1; i <= s.Length / 2; i++)
+            {
+                if (s.Length % i != 0)
+                    continue;
+
+                string subStr = s.Substring(0, i);
+
+                string[] split = s.Split(new[] { subStr }, StringSplitOptions.RemoveEmptyEntries);
+                if (split.Length == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
